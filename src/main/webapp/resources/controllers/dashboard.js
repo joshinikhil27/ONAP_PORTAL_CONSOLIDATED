1a18727a6ae6a9fe56aee00b496121f6cf8f7892
Abhijit <abhijitkalmegh9@gmail.com>
2017-10-03 15:23:08 +0530

    Update dashboard.js

--- src/main/webapp/resources/controllers/dashboard.js
+++ src/main/webapp/resources/controllers/dashboard.js
@@ -1,11 +1,13 @@
 app.controller('dashboardCtrl',['http_url','$scope','$http','$filter','$rootScope','$state',function(http_url,$scope,$http,$filter,$rootScope,$state){
-	  $scope.tabledata = []
+	$scope.showLoading = true;    
+	$scope.tabledata = []
 	  ,$scope.currentPage = 1
 	  ,$scope.numPerPage = 10
 	  ,$scope.maxSize = 5;
 	function getJson(){
 		$http.put(http_url+'demo/onap/ListRawData')
 	       .then(function(res){
+	    	   $scope.showLoading = false;
 	    	   $scope.tabledata = res.data;
 	    });
 	};
