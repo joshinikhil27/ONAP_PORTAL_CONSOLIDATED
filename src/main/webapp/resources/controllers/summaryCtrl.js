06aefd7cb4b57e082cfd56bffc5f4ad4bf6719d1
Abhijit <abhijitkalmegh9@gmail.com>
2017-10-03 15:26:47 +0530

    Update summaryCtrl.js

--- src/main/webapp/resources/controllers/summaryCtrl.js
+++ src/main/webapp/resources/controllers/summaryCtrl.js
@@ -1,9 +1,11 @@
 app.controller('summaryCtrl',['http_url','$scope','$http','$filter','$rootScope','$state',function(http_url,$scope,$http,$filter,$rootScope,$state){
+	$scope.showLoading = false;  
 	function getSummary(){
+		$scope.showLoading = true;
 		$http.put(http_url+'demo/onap/ListAuditInfo')
 	       .then(function(res){
+	    	   $scope.showLoading = false;
 	    	   $scope.summary = res.data;
-	    	   console.log('Summary '+JSON.stringify(res));
 	    });
 	};
 	getSummary();
