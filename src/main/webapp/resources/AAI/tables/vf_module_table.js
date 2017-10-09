51ee3e3da5a75db3202e0116cd4de3b090deb1ea
Abhijit <abhijitkalmegh9@gmail.com>
2017-10-09 16:38:01 +0530

    Add files via upload

---
+++ src/main/webapp/resources/AAI/tables/vf_module_table.js
@@ -0,0 +1,40 @@
+function vf_module_table(input) {
+	var tables = document.getElementsByTagName("TABLE");
+	for (var i=tables.length-1; i>=0;i-=1)
+  		if (tables[i]) tables[i].parentNode.removeChild(tables[i]);
+
+  	var table = document.createElement("TABLE");
+    table.setAttribute("id", "vf_module_table");
+    document.getElementById("newCust").appendChild(table);
+
+	var Th = document.createElement("TH");
+    Th.setAttribute("id", "myTh");
+    Th.colSpan = 2;
+    document.getElementById("vf_module_table").appendChild(Th);
+
+    var heading = document.createElement("TD");
+    heading.setAttribute("id", "TDH");
+    var heading_text = document.createTextNode("Vf-module : " + "USETE5MMUNJ0102UJZZ01-vjunos0" );
+    heading.appendChild(heading_text);
+    document.getElementById("myTh").appendChild(heading);
+
+    var count = 0;
+    for(var key in input){
+    	if(key != "vf-module-name"){
+	    	var y = document.createElement("TR");
+	    	y.setAttribute("id", count);
+	   		document.getElementById("vf_module_table").appendChild(y);
+
+	   		var key_value = document.createElement("TD");
+		    var key_data = document.createTextNode(key);
+		    key_value.appendChild(key_data);
+		    document.getElementById(count).appendChild(key_value);
+
+		    var value_value = document.createElement("TD");
+		    var value_data = document.createTextNode(input[key]);
+		    value_value.appendChild(value_data);
+		    document.getElementById(count).appendChild(value_value);
+		}
+	    count++;
+    }	
+}
